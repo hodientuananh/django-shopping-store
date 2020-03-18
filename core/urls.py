f7edb60a6b1bbd45e617532e72d915f9371efa04
minerva <anhhdt@minerva.vn>
2020-03-18 13:56:25 +0700

    add remove from cart

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import checkout_page, ItemDetailView, add_to_cart, HomeView
+from .views import checkout_page, ItemDetailView, add_to_cart, HomeView, remove_from_cart
 
 app_name = 'core'
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
     path("checkout", checkout_page, name="checkout"),
     path("product/<slug>/", ItemDetailView.as_view(), name="product"),
-    path('add_to_cart/<slug>/', add_to_cart, name="add-to-cart")
+    path('add_to_cart/<slug>/', add_to_cart, name="add-to-cart"),
+    path('remove_from_cart/<slug>/', remove_from_cart, name="remove-from-cart")
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, DetailView
 from django.utils import timezone
+from django.contrib import messages
 
 from .models import Item, Order, OrderItem
 
@@ -24,14 +25,36 @@ def add_to_cart(request, slug):
     if order_query_set.exists():
         order = order_query_set[0]
         if order.items.filter(item__slug=item.slug).exists():
+            messages.success(request, "Order item is increased to %s" % (order_item.quantity + 1))
             order_item.quantity += 1
             order_item.save()
         else:
+            messages.success(request, "Order item is added")
             order.items.add(order_item)
     else:
+        messages.success(request, "Order is added")
         ordered_date = timezone.now()
         order = Order.objects.create(user=request.user, ordered_date=ordered_date)
         order.items.add(order_item)
+    return redirect("core:product", slug=slug)
+
+
+def remove_from_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_query_set = Order.objects.filter(user=request.user, ordered=False)
+    if order_query_set.exists():
+        order = order_query_set[0]
+        if order.items.filter(item__slug=item.slug).exists():
+            messages.info(request, "Order is removed")
+            OrderItem.objects.filter(
+                item=item,
+                user=request.user,
+                ordered=False
+            )[0].delete()
+        else:
+            messages.info(request, "No order item can be removed now")
+    else:
+        messages.info(request, "No order can be removed now")
     return redirect("core:product", slug=slug)
 
 
