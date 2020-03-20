1af1eee436b0bfc8eecf330e92f87cb7a35f59e9
minerva <anhhdt@minerva.vn>
2020-03-20 11:26:37 +0700

    add correct static directory

--- django-shopping-store/settings.py
+++ django-shopping-store/settings.py
@@ -61,7 +61,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'core/static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
