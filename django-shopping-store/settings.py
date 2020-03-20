a371ac92bec11652d567f5d59841a5f89fdf69c6
minerva <anhhdt@minerva.vn>
2020-03-20 11:49:15 +0700

    add allowed host

--- django-shopping-store/settings.py
+++ django-shopping-store/settings.py
@@ -7,7 +7,7 @@ ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
 DEBUG = True
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fierce-ocean-22071.herokuapp.com']
 
 INSTALLED_APPS = [
     'django.contrib.sites',
