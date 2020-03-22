c92b99bdba556caa682bf323bc65cf881671424f
Tuan Anh Ho Dien <benkinmat@gmail.com>
2020-03-22 16:12:18 +0700

    remove redundant files

--- django-shopping-store/settings.py
+++ django-shopping-store/settings.py
@@ -33,7 +33,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'django-shopping-store.urls'
@@ -61,7 +60,8 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'core/static'),)
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'core/static'),)
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'core/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
